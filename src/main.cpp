a5c0f4d7e751a877395c53d9ab6732bfa39ada08
Cheeto836 <mprussell836@gmail.com>
2018-07-27 13:20:03 -0400

    several improvements to the file output. only the columns that get generated by the original ds log reader are printed.

--- src/main.cpp
+++ src/main.cpp
@@ -19,34 +19,35 @@ int main(int argc, char** argv)
   //TEMP
   DsLogReader reader("C:/Users/Public/Documents/FRC/Log Files/2018_04_25 19_43_46 Wed.dslog");
   std::ofstream file;
-  file.open("C:/Users/Cheeto/Robotics/FRC/Tools/Log-Analyzer/build/OutputFiles/Log.csv", std::iostream::out | std::iostream::app);
+  file.open("C:/Users/Cheeto/Robotics/FRC/Tools/Log-Analyzer/build/OutputFiles/Log.csv", std::iostream::out | std::iostream::trunc);
   if (file.is_open())
   {
     auto entries = reader.getEntries();
-    file << "Trip Time, Lost Packets, Voltage, RobotRIO CPU, CAN Utilization, wifi Db, bandwidth, PDP Resistance, PDP Voltage, PDP Temperature, Brownout, Watchdog, DS_Teleop, DS_Autonomous, DS_Disabled, Robot_Teleop, Robot_Autonomous, Robot_Disabled, ";
+    file << "Trip Time, Lost Packets, Voltage, RobotRIO CPU, CAN Utilization, ";
+    for (int i = 0; i < (int)DsLogEntry::STATUS_FLAGS::NUM_STATUS_FLAGS; i++)
+    {
+      file << DsLogEntry::STATUS_FLAG_STR[static_cast<DsLogEntry::STATUS_FLAGS>(i)] << ", ";
+    }
     for (int i = 0; i < entries[0].m_pdpCurrents.size(); i++)
     {
       file << "PDP[" << i << "], ";
     }
-    file << "Time" << std::endl;
+    file << std::endl;
     for (int i = 0; i < entries.size(); i++)
     {
       file << entries[i].m_tripTime << ", " << entries[i].m_lostPackets << ", "
         << entries[i].m_voltage << ", " << entries[i].m_roboRioCpu << ", "
-        << entries[i].m_canUtil << ", " << entries[i].m_wifiDb << ", "
-        << entries[i].m_bandwith << ", " << entries[i].m_pdpResistance << ", "
-        << entries[i].m_pdpVoltage << ", " << entries[i].m_pdpTemp << ", "
-        << entries[i].m_statusFlags[0] << ", " << entries[i].m_statusFlags[1] << ", "
-        << entries[i].m_statusFlags[2] << ", " << entries[i].m_statusFlags[3] << ", "
-        << entries[i].m_statusFlags[4] << ", " << entries[i].m_statusFlags[5] << ", "
-        << entries[i].m_statusFlags[6] << ", " << entries[i].m_statusFlags[7] << ", "
-        << entries[i].m_pdpId << ", ";
+        << entries[i].m_canUtil << ", " << entries[i].m_statusFlags[0] << ", " 
+        << entries[i].m_statusFlags[1] << ", " << entries[i].m_statusFlags[2] << ", " 
+        << entries[i].m_statusFlags[3] << ", " << entries[i].m_statusFlags[4] << ", " 
+        << entries[i].m_statusFlags[5] << ", " << entries[i].m_statusFlags[6] << ", " 
+        << entries[i].m_statusFlags[7] << ", ";
 
       for (int j = 0; j < entries[i].m_pdpCurrents.size(); j++)
       {
         file << entries[i].m_pdpCurrents[j] << ", ";
       } //end  for (int i = 0; i < entries[i].m_pdpCurrents.size(); i++)
-      file << entries[i].m_time.toString().toStdString() << std::endl;
+      file << std::endl;
     } //end  for (int i = 0; i < entries.size(); i++)
   } //end  if (file.is_open())
   else
